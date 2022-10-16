9972ced13a9889c44a93140f3ca2142e31b02ade
Ryan <ryanpjhickey@gmail.com>
2022-10-15 17:04:19 -0700

    update post route and also createNewnote function created

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,14 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const fs = require('fs');
+const path = require('path');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 
 
 app.get('/api/notes', (req, res) => {
@@ -19,10 +27,26 @@ app.get('/api/notes/:id', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    // req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validatenote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewnote(req.body, notes);
+        res.json(note);
+    }
 });
+function createNewnote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
